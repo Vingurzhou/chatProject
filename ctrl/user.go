7b606d65b7c6638bad59efa2941eb0c6bb66b7df
vingurzhou <zwz0123460218@icloud.com>
2022-07-15 20:36:59 +0800

    init

--- ctrl/user.go
+++ ctrl/user.go
@@ -1,15 +1,21 @@
 package ctrl
 
 import (
+	"chatProject/service"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+var userService service.UserService
+
 func UserLogin(c *gin.Context) {
-	// mobile := c.PostForm("mobile")
-	// passwd := c.PostForm("passwd")
+	mobile := c.PostForm("mobile")
+	passwd := c.PostForm("passwd")
+	user, err := userService.Login(mobile, passwd)
 	c.JSON(http.StatusOK, gin.H{
-		Code: code,
+		"code": 0,
+		"msg":  "密码不正确",
+		"data": "",
 	})
 }
