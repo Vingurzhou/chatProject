1c0ef50e7030c5b2cf76ebc8b80fa792c3ee1fa3
vingurzhou <zwz0123460218@icloud.com>
2022-07-15 20:37:09 +0800

    init

---
+++ service/init.go
@@ -0,0 +1,23 @@
+package service
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func init() {
+	dsn := "root:Z00a0319@tcp(139.196.137.117:4306)/gorm_test?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if nil != err && err.Error() != "" {
+		log.Fatal(err.Error())
+	}
+	DB = db
+	// DB.AutoMigrate(model.User{})
+
+	fmt.Println("init data base ok")
+}
--- service/user.go
+++ service/user.go
@@ -1,8 +1,11 @@
 package service
 
+import "chatProject/model"
+
 type UserService struct {
 }
 
-func (s *UserService) Login(mobile, plainpwd string) {
-
+func (s *UserService) Login(mobile, plainpwd string) (user model.User, err error) {
+	// tmp := model.User{}
+	// DB.Where("mobile = ?", mobile).Find(&tmp)
 }
